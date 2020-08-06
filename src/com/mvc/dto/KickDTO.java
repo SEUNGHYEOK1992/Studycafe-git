36fbf53cbd1a8bb3bf3b759a03dd5ad79098d01e
DESKTOP-SPB317A\gd <jhs0103857@naver.com>
2020-08-06 21:02:38 +0900

    관리자 서버 완료 (css,js 는 해야함) admin 컨트롤러,서비스,dao,dto 수정 report, kick, black DTO 추가

---
+++ src/com/mvc/dto/KickDTO.java
@@ -0,0 +1,29 @@
+package com.mvc.dto;
+
+public class KickDTO {
+	private int k_idx;
+	private String id;
+	private String cause;
+	
+	public int getK_idx() {
+		return k_idx;
+	}
+	public void setK_idx(int k_idx) {
+		this.k_idx = k_idx;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCause() {
+		return cause;
+	}
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+	
+	
+	
+}
