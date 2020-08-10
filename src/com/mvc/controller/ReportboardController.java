53fccdf8448986603f863c8af87a9c504d3704ab
박상현 <cmk051@naver.com>
2020-08-10 17:43:21 +0900

    공지사항 진행중 추가 커밋

---
+++ src/com/mvc/controller/ReportboardController.java
@@ -0,0 +1,44 @@
+package com.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mvc.service.ReportboardService;
+
+@WebServlet("/reportlist")
+public class ReportboardController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req,resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req,resp);
+	}
+
+	private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String uri = req.getRequestURI();
+		String ctx = req.getContextPath();
+		String  reqAddr = uri.substring(ctx.length());
+		
+		ReportboardService service = new ReportboardService(req,resp);
+		
+		switch(reqAddr) {
+		case "/reportlist":
+			System.out.println("공지사항 리스트 요청하기");
+			service.list();
+			break;
+			
+		}
+	}
+
+	
+}
---
+++ src/com/mvc/dao/ReportboardDAO.java
@@ -0,0 +1,69 @@
+package com.mvc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.mvc.dto.ReportboardDTO;
+
+public class ReportboardDAO {
+
+	Connection conn = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
+	
+	public ReportboardDAO() {
+		try {
+			Context ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/Oracle");
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public ArrayList<ReportboardDTO> list() {
+		String sql = "SELECT b_idx, id, subject content, reg_date, bHit FROM bbs WHERE id='admin' ORDER BY b_idx DESC";
+		ArrayList<ReportboardDTO> list = new ArrayList<ReportboardDTO>();
+		try {
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				ReportboardDTO dto = new ReportboardDTO();
+				dto.setB_idx(rs.getInt("b_idx"));
+				dto.setId(rs.getString("id"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setReg_date(rs.getDate("reg_date"));
+				dto.setbHit(rs.getInt("bHit"));
+				list.add(dto);
+			}System.out.println("reportboard dto 받아왓나?");
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			resClose();
+		}
+		return list;
+		
+	}
+
+	private void resClose() {
+		try {
+			if(rs!=null) {rs.close();}
+			if(ps!=null) {ps.close();}
+			if(conn!=null) {conn.close();}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
